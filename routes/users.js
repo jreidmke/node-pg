235da1f28952e6ea933f4752d15cb964d0eac79e
unknown <jreidmke@gmail.com>
2020-10-21 14:06:09 -0500

    new routing for CRUD"

--- routes/users.js
+++ routes/users.js
@@ -89,5 +89,45 @@ router.get('/', async(req, res, next) => {
   }
 })
 
+router.get('/search', async(req, res, next) => {
+  try {
+    const type = req.query.type; //type is a column in our users tables
+    // const results = await db.query(`SELECT * FROM users WHERE type=$1'${type}'`); //you need to have qutoes around 'type' so it is read as a string and not a var
+    const results = await db.query(`SELECT * FROM users WHERE type=$1`, [type]); //this one uses sanitized queries.
+
+    return res.json(results.rows);
+  } catch (error) {
+    next(error);
+  }
+})
+
+router.post('/', async(req, res, next) => {
+  try {
+    const {name, type} = req.body; //use object destructuring to isolate name and type properties from json body.
+    const results = await db.query(`INSERT INTO users (name, type) VALUES($1, $2) RETURNING *`, [name, type]); //insert them into the table along with a RETURNING which spits out name and type data
+    return res.status(201).json(results.rows); //pass along 201 (created) status
+  } catch (error) {
+    next(error);
+  }
+})
+
+router.patch('/:id', async(req, res, next) => {
+  try {
+    const {name, type} = req.body;
+    const results = await db.query(`UPDATE users SET name=$1, type=$2 WHERE id=$3 RETURNING *`, [name, type, req.params.id])//remember, id is stored in req.params
+    return res.json(results.rows);
+  } catch (error) {
+    next(error);
+  }
+})
+
+router.delete('/:id', async(req, res, next)=> {
+  try {
+    const results = await db.query(`DELETE FROM users WHERE id=$1 RETURNING 'User Deleted'`, [req.params.id]);
+    return res.json(results.rows)
+  } catch (error) {
+    next(error);
+  }
+})
 
 module.exports = router;
